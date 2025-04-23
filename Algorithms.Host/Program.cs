8a32bd5af92c2a3521525938568e803e566b4233
05anton05 <Anton05Postavets@gmail.com>
2025-04-23 20:58:35 +0300

    Add a text

--- Algorithms.Host/Program.cs
+++ Algorithms.Host/Program.cs
@@ -4,4 +4,4 @@ using Microsoft.Extensions.DependencyInjection;
 var serviceProvider = new ServiceCollection()
 .RegisterServices()
 .BuildServiceProvider();
-Console.WriteLine();
+Console.WriteLine("text");
